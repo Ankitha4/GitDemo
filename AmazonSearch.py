4141b96ec15575b24a97b92b6692bf49ede49c8b
Ankitha4 <anki.shetty20@gmail.com>
2025-09-10 16:38:36 +0530

    first

---
+++ AmazonSearch.py
@@ -0,0 +1,13 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.amazon.in/")
+driver.maximize_window()
+time.sleep(5)
+driver.find_element(By.XPATH,"//input[@placeholder='Search Amazon.in']").send_keys("iphone")
+driver.find_element(By.XPATH,"input[@name='UserFirstName']").send_keys("Ank")
+driver.find_element(By.ID,"nav-search-submit-button").click()
+time.sleep(5)
